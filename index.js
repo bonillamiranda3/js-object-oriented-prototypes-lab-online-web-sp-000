919ea019dbdabbcead160894646af7d8f63b0625
Miranda Bonilla <bonillamiranda3@gmail.com>
2019-03-18 01:34:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,17 +4,17 @@
    this.training = training
 }
    BoardMember.protoype.veto = function(){
-     return 'No, I must disagree'
+     return 'No, I must disagree';
    }
    BoardMemeber.protoype.approve = function(){
-     return 'You can do that!'
+     return 'You can do that!';
    }
    BoardMemeber.protoype.doCharity = function(){
-     return "I like to help people."
+     return "I like to help people.";
    }
 
    BoardMemeber.protoype.releasePressStatement = function(){
-     return "You will see great things from Scuber."
+     return "You will see great things from Scuber.";
    }
 
    BoardMemeber.protoype.sayHi = function(){
